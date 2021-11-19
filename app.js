02835d3b2c1bc8b73496bb39d83f6f351128e591
emilyanneboj <94072490+emilyanneboj@users.noreply.github.com>
2021-11-19 09:57:15 -0800

    Added Fahrenheit  first

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates){
     let apiKey = "57e6867ef7f91c4fc24cfd282653138f";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(displayForecast);
 }
 
@@ -83,7 +83,7 @@ function displayTemperature(response){
 
 function search(city) { 
 let apiKey = "57e6867ef7f91c4fc24cfd282653138f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 axios.get(apiUrl).then(displayTemperature);
 }
 
